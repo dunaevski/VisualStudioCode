d7ab5c5631786711cc8b8616334a8260cadc5ba8
yaltasashay <yaltasashay@gmail.com>
2017-11-29 17:23:15 +0300

    Close

--- source/repos/TN/Array/Program.cs
+++ source/repos/TN/Array/Program.cs
@@ -85,7 +85,22 @@ namespace ConsoleApp2
             Console.WriteLine("Enter index of array");
             int size = int.Parse(Console.ReadLine());
             int[] arrput = new int[size];
-            
+
+
+            Console.WriteLine("Two Dimensional Array");
+            int[,] matrix1 = new int[4, 4];
+            int[][] matrix2 = new int[5][]; // неровные массивы 
+            int[,] matrix3 = { { 1, 2, 3 }, { 3, 5, 6 } };
+
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrix1[i, j] = random.Next(20);
+                    Console.Write(matrix1[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
 
 
             Console.ReadLine();
--- source/repos/TN/twoDimensionalArray/Program.cs
+++ source/repos/TN/twoDimensionalArray/Program.cs
@@ -10,20 +10,7 @@ namespace twoDimensionalArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Two Dimensional Array");
-            int[,] matrix1 = new int[4, 4];
-            int[][] matrix2 = new int[5][]; // неровные массивы 
-            int[,] matrix3 = {{1, 2, 3}, {3, 5, 6}};
-
-            for (int i = 0; i < matrix1.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix1.GetLength(1); j++)
-                {
-                    
-                }
-            }
-
-
+         
             Console.ReadLine();
         }
 
