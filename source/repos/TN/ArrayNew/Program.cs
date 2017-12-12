3522577139527225dda154eb582838023808f40a
yaltasashay <yaltasashay@gmail.com>
2017-12-13 00:27:44 +0300

    Clear

---
+++ source/repos/TN/ArrayNew/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ArrayNew
+{
+    class Program
+    {
+        private static void Return()
+        {
+            const int size = 2018;
+            var arrInts = new int[120];
+            var number = new int[size];
+            var index = 0;
+            for (int i = 0; i < size; i++)
+            {
+                number[i] = i + 1;
+                var temp = number[i];
+                var sum = 0;
+                while (temp != 0)
+                {
+                    sum += temp % 10;
+                    temp /= 10;
+                }
+                if (sum == 10)
+                {
+                    arrInts[index] = number[i];
+                    index++;
+                    if (index+1%12==0)
+                        Console.WriteLine("\n");
+                    else
+                        Console.Write(number[i] + "\t");
+                }
+                if (index == 120)
+                    break;
+            }
+        }
+
+        static void Main()
+        {
+            Return();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/repos/TN/Key/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Key
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("решИла нЕ Упрощать и зашифРОВАтЬ Все послаНИЕ" +
+                                                 " дАже не Старайся нИЧЕГО у тЕбя нЕ получится с расшифРОВкой" +
+                                                 " Сдавайся НЕ твоего ума Ты не споСОбЕн Но может быть" +
+                                                 " если особенно упорно подойдешь к делу" +
+                                                 " будет Трудно конечнО" +
+                                                 " Код ведЬ не из простых" +
+                                                 " очень ХОРОШИЙ код" +
+                                                 " то у тебя все получится" +
+                                                 " и я буДу Писать тЕбЕ еще" +
+                                                 " чао\n");
+            string sbs = sb.ToString();
+            Console.WriteLine(sbs);
+            string text = " ";
+            Regex regA = new Regex(@"\s[А-Я]\S*");
+            foreach (var i in regA.Matches(sbs))
+                text += i;
+            string[] words = text.Split(' ');
+            Array.Reverse(words);
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i] + " ");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
