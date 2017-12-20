8cb6d9021fdecb4dc367b4435c27efd7a0ca460a
yaltasashay <yaltasashay@gmail.com>
2017-12-20 17:19:55 +0300

    Summator

---
+++ source/repos/TN/Params/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Program
+    {
+        static int minVal(params int[] num)
+        {
+            int min;
+            if (num.Length == 0)
+            {
+                Console.WriteLine("Ne ukazan argument");
+                return 0;
+            }
+            else
+            {
+                min = num[0];
+                foreach (int t in num)
+                {
+                    if (t<min)
+                        min = t;
+                }
+                return min;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(minVal(3,4,-5,8));
+            Console.WriteLine(minVal(17,12,87));
+            Console.WriteLine(minVal());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/repos/TN/Sumator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumator
+{
+    class Sumator
+    {
+        public int summa;
+
+        public Sumator(int num)
+        {
+            summa = 0;
+            for (int i = 0; i < num; i++)
+                summa += i;
+        }
+
+        public Sumator(Sumator obj)
+        {
+            summa = obj.summa;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
