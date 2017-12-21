88d1ba1cb13638fb480318971a7a345d25916df1
yaltasashay <yaltasashay@gmail.com>
2017-12-21 16:11:28 +0300

    DemoArray

--- source/repos/TN/DemoMyArray/Program.cs
+++ source/repos/TN/DemoMyArray/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,7 +34,7 @@ namespace DemoMyArray
                 if (Ok(index))
                 {
                     errFlag = false;
-                    return a[index]
+                    return a[index];
                 }
                 else
                 {
@@ -52,7 +53,7 @@ namespace DemoMyArray
                     errFlag = true;
             }
         }
-    }  
+    }
 
     class Program
     {
@@ -62,9 +63,30 @@ namespace DemoMyArray
             Console.WriteLine("Использование метода set индексатора: ");
             for (int i = 0; i < array.length; i++)
             {
-                array[i] = Math.Sqrt(i);
+                array[i] = Math.Sqrt(i)*10;
+                Console.WriteLine("{0:00.000}", array[i]);
+                if (array.errFlag)
+                    Console.WriteLine("Element array[" + i + "] - ошибка выхода за границы");
             }
+            double x;
+            int index;
+            Console.WriteLine();
+            Console.WriteLine("Использование метода get индексатора: ");
+            Console.Write("Enter index of element's ");
+            index = Convert.ToInt32(Console.ReadLine());
+
+            x = array[index];
+            if (!array.errFlag)
+            {
+                Console.WriteLine("{0:00.000}", x);
+            }
+            else
+                Console.WriteLine("Element array[" + index + "] - ошибка выхода за границы");
+
+            Console.ReadKey();
         }
     }
-    }
 }
+
+
+
