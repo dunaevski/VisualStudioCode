8d800ee3d9a1bce9844c07ee81caaeec700da640
yaltasashay <yaltasashay@gmail.com>
2017-12-22 16:56:00 +0300

    Done

---
+++ source/repos/TN/Operator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    internal class ThreeD
+    {   
+        private int _x, _y, _z;
+
+        public ThreeD(int x = 0, int y = 0, int z = 0)
+        {
+            this._x = x;
+            this._y = y;
+            this._z = z;
+        }
+    
+        public static ThreeD operator +(ThreeD obj1, ThreeD obj2)
+        {
+            var arr = new ThreeD
+            {
+                _x = obj1._x + obj2._x,
+                _y = obj1._y + obj2._y,
+                _z = obj1._z + obj2._z
+            };
+            return arr;
+        }
+
+        public static ThreeD operator -(ThreeD obj1, ThreeD obj2)
+        {
+            var arr = new ThreeD
+            {
+                _x = obj1._x - obj2._x,
+                _y = obj1._y - obj2._y,
+                _z = obj1._z - obj2._z
+            };
+            return arr;
+        }
+
+        public static ThreeD operator ++(ThreeD obj1)
+        {
+            obj1._x++;
+            obj1._y++;
+            obj1._z++;
+            return obj1;
+        }
+
+        public static ThreeD operator --(ThreeD obj1)
+        {
+            obj1._x--;
+            obj1._y--;
+            obj1._z--;
+            return obj1;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Координаты точки: " +
+                             _x + " " + _y + " " + _z);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point1 = new ThreeD(1, 12, -4);
+            point1.Show();
+            var point2 = new ThreeD(0, -3, 18);
+            point2.Show();
+            var point3 = point1 + point2;
+            point3.Show();
+            point3 = point1 - point2;
+            point3.Show();
+            point3++;
+            point3.Show();
+            point3--;
+            point3.Show();
+            var point4 = new ThreeD();
+            point4.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
--- source/repos/TN/Task_2/Program.cs
+++ source/repos/TN/Task_2/Program.cs
@@ -14,15 +14,19 @@ namespace Task_2
             var n = int.Parse(Console.ReadLine());
             var random =new Random();
             var a = new int[n];
+            // var a = new int[9] {2,1,3,8,1,1,2,8,9};
+        
             var count = new int[a.Length];
-            for (var i = 0; i < a.Length; i++)
-                a[i] = random.Next(10);
+              for (var i = 0; i < a.Length; i++)
+                  a[i] = random.Next(5); 
+
+
             for (var index = 0; index < a.Length; index++)
             {
                 count[index] = a.Count(t => a[index] == t);
                 Console.Write(a[index] + " ");
             }
-            Console.WriteLine("\n{0} повторяется {1} раз", a[count.Max()], count.Max());
+            Console.WriteLine("\n{0} повторяется {1} раз",a[count.ToList().IndexOf(count.Max())], count.Max());
             Console.ReadKey();
         }
     }
